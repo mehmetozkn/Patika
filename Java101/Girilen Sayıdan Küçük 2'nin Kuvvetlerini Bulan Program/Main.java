40ec3bfe1b6688cb2c55cb1d73963502c150377f
Mehmet Özkan <mehmet61ozkan@outlook.com>
2021-10-11 23:21:30 +0300

    .

---
+++ Java101/Girilen Sayıdan Küçük 2'nin Kuvvetlerini Bulan Program/Main.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws ArithmeticException {
+		
+			Scanner sc = new Scanner(System.in);
+			
+			int number = 0;
+			System.out.print("Number : ");
+			number = sc.nextInt();
+			
+			for(int i = 1 ; i <= number ; i*=2) System.out.println(i);
+				
+			
+			
+			
+			
+			
+	
+		
+	    }
+	  }
