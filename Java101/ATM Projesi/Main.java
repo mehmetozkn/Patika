c5a41783d8bf2fe7642d7e236d0a60e477a419a9
Mehmet Özkan <mehmet61ozkan@outlook.com>
2021-10-12 14:50:26 +0300

    .

---
+++ Java101/ATM Projesi/Main.java
@@ -0,0 +1,75 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws ArithmeticException {
+
+		Scanner sc = new Scanner(System.in);
+
+		String userName, password;
+		Scanner input = new Scanner(System.in);
+		int right = 3;
+		int balance = 1500;
+		int price;
+		int select = 0;
+		System.out.print("Kullanıcı Adınız :");
+		userName = input.nextLine();
+		System.out.print("Parolanız : ");
+		password = input.nextLine();
+		while (right > 0) {
+			
+
+			if (userName.equals("patika") && password.equals("dev123")) {
+				System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+
+				System.out.println("1-Para yatırma\n" + "2-Para Çekme\n" + "3-Bakiye Sorgula\n" + "4-Çıkış Yap");
+				System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz : ");
+				select = input.nextInt();
+
+				switch (select) {
+
+				case 1:
+					System.out.print("Para miktarı : ");
+					price = input.nextInt();
+					balance += price;
+
+				case 2:
+					System.out.print("Para miktarı : ");
+					price = input.nextInt();
+					if (price > balance) {
+						System.out.println("Bakiye yetersiz.");
+						break;
+					} else {
+						balance -= price;
+						break;
+					}
+
+				case 3:
+					System.out.println("Bakiyeniz : " + balance);
+					break;
+
+				case 4:
+					System.out.println("Tekrar görüşmek üzere.");
+					break;
+					
+				}
+
+			} else {
+				right--;
+				System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+				if (right == 0) {
+					System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
+				} else {
+					System.out.println("Kalan Hakkınız : " + right);
+				}
+			}
+			if(select == 4) {
+				break;
+			}
+		}
+
+	}
+
+}
